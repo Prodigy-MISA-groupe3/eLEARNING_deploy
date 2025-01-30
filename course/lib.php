c4a657418778c68c7d51073fb8a3d5d735187987
Derick Turner <derick@e-learndesign.co.uk>
2025-01-30 14:57:49 +0000

    IOMAD: Courses from other company being listed in navigation/other places - closes #2195

--- course/lib.php
+++ course/lib.php
@@ -4634,6 +4634,9 @@ function course_get_recent_courses(?int $userid = null, int $limit = 0, int $off
         });
     }
 
+    // IOMAD - we want to only show courses for the current company
+    $recentcourses = iomad::iomad_filter_courses($recentcourses);
+
     return $recentcourses;
 }
 
