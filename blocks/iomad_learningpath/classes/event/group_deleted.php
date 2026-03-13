iomad/iomad

IOMAD: Searching/adding users to a learning path is not great and should up updated to use standard user selector type forms. Adding someone to a path doesn't do anything with respect to enrolment, so manual enrolment courses are assigned when the user is added to a path, self enrolment coures are left as is.  Overall calculation is based on the number of path groups not the total number of courses - which is causing confusion. Lastly - the UI could do with a tidy-up - closes #2612 #2524

blocks/iomad_learningpath/classes/event/group_deleted.php
@@ -1,0 +1,107 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The block_iomad_learningpath user assigned event.
+ *
+ * @package    block_iomad_learningpath
+ * @copyright  2026 E-Learn Design Ltd. http://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_iomad_learningpath\event;
+
+use core\event\base;
+use core\exception\coding_exception;
+use moodle_url;
+
+/**
+ * The block_iomad_learningpath user assigned event.
+ *
+ * @package    block_iomad_learningpath
+ * @copyright  2026 E-Learn Design Ltd. http://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class group_deleted extends base {
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'c';
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+        $this->data['objecttable'] = 'iomad_learningpathgroup';
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('sectiondeleted', 'block_iomad_learningpath');
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "The user with id '$this->userid' deleted the learning path section with id " .
+               "'$this->objectid' for the learning path with id '" . $this->other['learningpathid'] . "'";
+    }
+
+    /**
+     * Get URL related to the action.
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new moodle_url('/local/iomad_learningpath/courselist.php');
+    }
+
+    /**
+     * Custom validation.
+     *
+     * @throws coding_exception
+     * @return void
+     */
+    protected function validate_data() {
+        parent::validate_data();
+
+        if (!array_key_exists('learningpathid', $this->other)) {
+            throw new coding_exception('The \'learningpathid\' value must be set in other.');
+        }
+    }
+
+    /**
+     * Define the mappings for the event other array
+     *
+     * @return void
+     */
+    public static function get_other_mapping() {
+        $othermapped = [];
+
+        $othermapped['learningpathid'] = ['db' => 'iomad_learningpath'];
+
+        return $othermapped;
+    }
+}

blocks/iomad_learningpath/classes/event/group_updated.php
@@ -1,0 +1,107 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The block_iomad_learningpath user assigned event.
+ *
+ * @package    block_iomad_learningpath
+ * @copyright  2026 E-Learn Design Ltd. http://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_iomad_learningpath\event;
+
+use core\event\base;
+use core\exception\coding_exception;
+use moodle_url;
+
+/**
+ * The block_iomad_learningpath user assigned event.
+ *
+ * @package    block_iomad_learningpath
+ * @copyright  2026 E-Learn Design Ltd. http://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class group_updated extends base {
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'c';
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+        $this->data['objecttable'] = 'iomad_learningpathgroup';
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('sectionupdated', 'block_iomad_learningpath');
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "The user with id '$this->userid' updated the learning path section with id " .
+               "'$this->objectid' for the learning path with id '" . $this->other['learningpathid'] . "'";
+    }
+
+    /**
+     * Get URL related to the action.
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new moodle_url('/local/iomad_learningpath/courselist.php');
+    }
+
+    /**
+     * Custom validation.
+     *
+     * @throws coding_exception
+     * @return void
+     */
+    protected function validate_data() {
+        parent::validate_data();
+
+        if (!array_key_exists('learningpathid', $this->other)) {
+            throw new coding_exception('The \'learningpathid\' value must be set in other.');
+        }
+    }
+
+    /**
+     * Define the mappings for the event other array
+     *
+     * @return void
+     */
+    public static function get_other_mapping() {
+        $othermapped = [];
+
+        $othermapped['learningpathid'] = ['db' => 'iomad_learningpath'];
+
+        return $othermapped;
+    }
+}

local/iomad/classes/user_selector/current_learningpath.php
@@ -1,0 +1,138 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local IOMAD current learningpath user selector class
+ *
+ * @package   local_iomad
+ * @copyright 2021 Derick Turner
+ * @author    Derick Turner
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_iomad\user_selector;
+
+use company;
+use company_user_selector_base;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/blocks/iomad_company_admin/lib/user_selectors.php');
+
+/**
+ * Local IOMAD current learningpath user selector class
+ *
+ * @package   local_iomad
+ * @copyright 2021 Derick Turner
+ * @author    Derick Turner
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class current_learningpath extends company_user_selector_base {
+
+    /** @var int learningpath id */
+    protected $pathid;
+
+    /**
+     * Constructor function
+     *
+     * @param string $name
+     * @param array $options
+     */
+    public function __construct($name, $options) {
+        $this->pathid = !empty($options['pathid']) ? $options['pathid'] : 0;
+        parent::__construct($name, $options);
+    }
+
+    /**
+     * Get selector options
+     *
+     * @return array
+     */
+    protected function get_options() {
+        $options = parent::get_options();
+        $options['pathid'] = $this->pathid;
+        $options['file'] = 'local/iomad/classes/user_selector/current_learningpath.php';
+
+        return $options;
+    }
+
+    /**
+     * Find company users assigned to microlearning learningpath
+     * @param string $search
+     * @return array
+     */
+    public function find_users($search, $all = false) {
+        global $CFG, $DB;
+
+        // By default wherecondition retrieves all users except the deleted, not confirmed and guest.
+        $this->searchtype = USER_SEARCH_CONTAINS;
+        list($wherecondition, $params) = $this->search_sql($search, 'u');
+        $params['companyid'] = $this->companyid;
+        $params['pathid'] = $this->pathid;
+
+        // Deal with departments.
+        $departmentlist = company::get_all_subdepartments($this->departmentid);
+        $departmentsql = "";
+        if (!empty($departmentlist)) {
+            [$insql, $inparams] = $DB->get_in_or_equal(array_keys($departmentlist),
+                                                       SQL_PARAMS_NAMED,
+                                                       'deptids');
+            $departmentsql = " AND cu.departmentid {$insql}";
+            $params = $params + $inparams;
+        }
+
+        $fields = 'SELECT DISTINCT ' . $this->required_fields_sql('u');
+        $countfields = 'SELECT COUNT(DISTINCT u.id)';
+
+        $sql = " FROM {user} u
+                 JOIN {company_users} cu ON (cu.userid = u.id)
+                 LEFT JOIN {user_info_data} ui ON (
+                     ui.userid = u.id
+                     AND ui.userid = cu.userid
+                 )
+                 WHERE $wherecondition
+                 AND u.suspended = 0
+                 AND cu.companyid = :companyid
+                 $departmentsql
+                 AND cu.userid IN (
+                   SELECT DISTINCT userid
+                   FROM {iomad_learningpathuser}
+                   WHERE pathid = :pathid
+                 )";
+
+        $order = ' ORDER BY u.lastname ASC, u.firstname ASC';
+
+        // Do we get too many results?
+        if (!$this->is_validating() && !$all) {
+            $potentialmemberscount = $DB->count_records_sql($countfields . $sql, $params);
+            if ($potentialmemberscount > $CFG->iomad_max_select_users) {
+                return $this->too_many_results($search, $potentialmemberscount);
+            }
+        }
+
+        // Get the list of users.
+        $availableusers = $DB->get_records_sql($fields . $sql . $order, $params);
+
+        // Deal with any search text.
+        if ($search) {
+            $groupname = get_string('curusersmatching', 'block_iomad_company_admin', $search);
+        } else {
+            $groupname = get_string('curusers', 'block_iomad_company_admin');
+        }
+
+        return [$groupname => $availableusers];
+    }
+}

Prodigy-MISA-groupe3/eLEARNING_deploy

blocks/iomad_learningpath/classes/event/group_deleted.php
@@ -1,0 +1,107 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The block_iomad_learningpath user assigned event.
+ *
+ * @package    block_iomad_learningpath
+ * @copyright  2026 E-Learn Design Ltd. http://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_iomad_learningpath\event;
+
+use core\event\base;
+use core\exception\coding_exception;
+use moodle_url;
+
+/**
+ * The block_iomad_learningpath user assigned event.
+ *
+ * @package    block_iomad_learningpath
+ * @copyright  2026 E-Learn Design Ltd. http://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class group_deleted extends base {
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'c';
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+        $this->data['objecttable'] = 'block_iomad_learningpath_groups';
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('sectiondeleted', 'block_iomad_learningpath');
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "The user with id '$this->userid' deleted the learning path section with id " .
+               "'$this->objectid' for the learning path with id '" . $this->other['pathid'] . "'";
+    }
+
+    /**
+     * Get URL related to the action.
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new moodle_url('/local/iomad_learningpath/courselist.php');
+    }
+
+    /**
+     * Custom validation.
+     *
+     * @throws coding_exception
+     * @return void
+     */
+    protected function validate_data() {
+        parent::validate_data();
+
+        if (!array_key_exists('pathid', $this->other)) {
+            throw new coding_exception('The \'pathid\' value must be set in other.');
+        }
+    }
+
+    /**
+     * Define the mappings for the event other array
+     *
+     * @return void
+     */
+    public static function get_other_mapping() {
+        $othermapped = [];
+
+        $othermapped['pathid'] = ['db' => 'block_iomad_learningpath'];
+
+        return $othermapped;
+    }
+}

blocks/iomad_learningpath/classes/event/group_updated.php
@@ -1,0 +1,107 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The block_iomad_learningpath user assigned event.
+ *
+ * @package    block_iomad_learningpath
+ * @copyright  2026 E-Learn Design Ltd. http://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_iomad_learningpath\event;
+
+use core\event\base;
+use core\exception\coding_exception;
+use moodle_url;
+
+/**
+ * The block_iomad_learningpath user assigned event.
+ *
+ * @package    block_iomad_learningpath
+ * @copyright  2026 E-Learn Design Ltd. http://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class group_updated extends base {
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'c';
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+        $this->data['objecttable'] = 'block_iomad_learningpath_groups';
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('sectionupdated', 'block_iomad_learningpath');
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "The user with id '$this->userid' updated the learning path section with id " .
+               "'$this->objectid' for the learning path with id '" . $this->other['pathid'] . "'";
+    }
+
+    /**
+     * Get URL related to the action.
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new moodle_url('/local/iomad_learningpath/courselist.php');
+    }
+
+    /**
+     * Custom validation.
+     *
+     * @throws coding_exception
+     * @return void
+     */
+    protected function validate_data() {
+        parent::validate_data();
+
+        if (!array_key_exists('pathid', $this->other)) {
+            throw new coding_exception('The \'pathid\' value must be set in other.');
+        }
+    }
+
+    /**
+     * Define the mappings for the event other array
+     *
+     * @return void
+     */
+    public static function get_other_mapping() {
+        $othermapped = [];
+
+        $othermapped['pathid'] = ['db' => 'block_iomad_learningpath'];
+
+        return $othermapped;
+    }
+}

public/local/iomad/classes/user_selector/current_learningpath.php
@@ -1,0 +1,133 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local IOMAD current learningpath user selector class
+ *
+ * @package   local_iomad
+ * @copyright 2021 Derick Turner
+ * @author    Derick Turner
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_iomad\user_selector;
+
+use local_iomad\company;
+
+/**
+ * Local IOMAD current learningpath user selector class
+ *
+ * @package   local_iomad
+ * @copyright 2021 Derick Turner
+ * @author    Derick Turner
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class current_learningpath extends company_base {
+
+    /** @var int learningpath id */
+    protected $pathid;
+
+    /**
+     * Constructor function
+     *
+     * @param string $name
+     * @param array $options
+     */
+    public function __construct($name, $options) {
+        $this->pathid = !empty($options['pathid']) ? $options['pathid'] : 0;
+        parent::__construct($name, $options);
+    }
+
+    /**
+     * Get selector options
+     *
+     * @return array
+     */
+    protected function get_options() {
+        $options = parent::get_options();
+        $options['pathid'] = $this->pathid;
+        $options['file'] = 'local/iomad/classes/user_selector/current_learningpath.php';
+
+        return $options;
+    }
+
+    /**
+     * Find company users assigned to microlearning learningpath
+     * @param string $search
+     * @return array
+     */
+    public function find_users($search, $all = false) {
+        global $DB;
+
+        // By default wherecondition retrieves all users except the deleted, not confirmed and guest.
+        $this->searchtype = USER_SEARCH_CONTAINS;
+        list($wherecondition, $params) = $this->search_sql($search, 'u');
+        $params['companyid'] = $this->companyid;
+        $params['pathid'] = $this->pathid;
+
+        // Deal with departments.
+        $departmentlist = company::get_all_subdepartments($this->departmentid);
+        $departmentsql = "";
+        if (!empty($departmentlist)) {
+            [$insql, $inparams] = $DB->get_in_or_equal(array_keys($departmentlist),
+                                                       SQL_PARAMS_NAMED,
+                                                       'deptids');
+            $departmentsql = " AND cu.departmentid {$insql}";
+            $params = $params + $inparams;
+        }
+
+        $fields = 'SELECT DISTINCT ' . $this->required_fields_sql('u');
+        $countfields = 'SELECT COUNT(DISTINCT u.id)';
+
+        $sql = " FROM {user} u
+                 JOIN {local_iomad_company_users} cu ON (cu.userid = u.id)
+                 LEFT JOIN {user_info_data} ui ON (
+                     ui.userid = u.id
+                     AND ui.userid = cu.userid
+                 )
+                 WHERE $wherecondition
+                 AND u.suspended = 0
+                 AND cu.companyid = :companyid
+                 $departmentsql
+                 AND cu.userid IN (
+                   SELECT DISTINCT userid
+                   FROM {block_iomad_learningpath_users}
+                   WHERE pathid = :pathid
+                 )";
+
+        $order = ' ORDER BY u.lastname ASC, u.firstname ASC';
+
+        // Do we get too many results?
+        if (!$this->is_validating() && !$all) {
+            $potentialmemberscount = $DB->count_records_sql($countfields . $sql, $params);
+            if ($potentialmemberscount > get_config('local_iomad', 'max_select_users')) {
+                return $this->too_many_results($search, $potentialmemberscount);
+            }
+        }
+
+        // Get the list of users.
+        $availableusers = $DB->get_records_sql($fields . $sql . $order, $params);
+
+        // Deal with any search text.
+        if ($search) {
+            $groupname = get_string('curusersmatching', 'block_iomad_company_admin', $search);
+        } else {
+            $groupname = get_string('curusers', 'block_iomad_company_admin');
+        }
+
+        return [$groupname => $availableusers];
+    }
+}